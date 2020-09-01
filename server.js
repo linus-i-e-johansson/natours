4fb5b1536524520bc91a18b9810f630be794c8f1
Linus <linus.react.projects@gmail.com>
2020-09-01 15:18:41 +0200

    cleanup code

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 //staring file
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+const DB = process.env.DATABASE;
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection successful!"));
 // 1) START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
