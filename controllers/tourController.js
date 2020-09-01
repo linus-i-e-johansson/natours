aba76184b41daf3985c1b9f74fd052d6ea4aa2c9
Linus <linus.react.projects@gmail.com>
2020-09-02 00:40:43 +0200

    fixed create async func for the API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,13 +25,25 @@ exports.getSingelTour = (req, res) => {
     },
   });*/
 };
+
 //Add a new tour.
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    /*data: { tour: newTour },*/
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
 };
+
 // UPDATE a tour
 exports.updateTour = (req, res) => {
   res.status(200).json({
