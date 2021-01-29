3d56d7e7ac86c47f18aff37bebec628c656d923e
Linus Johansson <linus.react.projects@gmail.com>
2021-01-29 16:40:53 +0100

    added better field limitation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,13 +17,22 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(JSON.parse(queryString));
     // Tour.find() returns a query, so that we can keep chaining methods to it.
 
-    // 2) Sorting
+    // 2.) Sorting
     if (req.query.sort){
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     }else{
       query= query.sort("-createdAt");
     }
+
+    // 3.) Field limiting
+    if(req.query.fields){
+      const fields = req.query.fields.split(",").join(" ");
+      query =  query.select(fields);
+    }else {
+      query.select("-__v")
+    }
+
 
     //EXECUTE QUERY
     const tours = await query;
