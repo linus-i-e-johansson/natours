eeddd35ba2748350afc06ddd8db8c600f72cea88
Linus Johansson <linus.react.projects@gmail.com>
2021-02-05 10:51:06 +0100

    simple halfway save

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -159,7 +159,22 @@ exports.getMonthlyPlan = async (req,res) => {
   try {
     const year = req.params.year*1;
     const plan = await Tour.aggregate([
-      {$unwind:"$startDates"}
+      { $unwind: "$startDates" },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
     ]);
 
     res.status(201).json({
