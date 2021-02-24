cffb0818577636a38cf550a2a30d7abf3e7d73d6
Linus Johansson <linus.react.projects@gmail.com>
2021-02-24 14:47:33 +0100

    added error handling middleware

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.use(express.json()); // middleware, needed so that the data from the body is
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware ;)");
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,5 +23,26 @@ app.use((req, res, next) => {
 // 2) ROUTES
 app.use("/api/v1/tours", tourRouter); // mounting the router
 app.use("/api/v1/users", userRouter); // mounting the router
+
+app.all("*",(req, res, next) => {
+  /*res.status(404).json({
+    status: "fail",
+    message: `Cant find ${req.originalUrl} on this server!`
+  });*/
+  const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err,req,res,next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message:err.message
+  });
+})
 
 module.exports = app;
