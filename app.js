40c9e1cc2109c877dbf4eab795679b0cc5cf8822
Linus Johansson <linus.react.projects@gmail.com>
2021-02-24 15:06:05 +0100

    added errorcontroller file and refactored errors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController")
 const app = express();
 
 //1) MIDDLEWARES
@@ -25,24 +27,12 @@ app.use("/api/v1/tours", tourRouter); // mounting the router
 app.use("/api/v1/users", userRouter); // mounting the router
 
 app.all("*",(req, res, next) => {
-  /*res.status(404).json({
-    status: "fail",
-    message: `Cant find ${req.originalUrl} on this server!`
-  });*/
+/*
   const err = new Error(`Cant find ${req.originalUrl} on this server!`);
   err.status = "fail";
   err.statusCode = 404;
-  next(err);
+  next(err);*/
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`,404));
 });
-
-app.use((err,req,res,next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message:err.message
-  });
-})
-
+app.use(globalErrorHandler)
 module.exports = app;
---
+++ utils/appError.js
@@ -0,0 +1,11 @@
+class AppError extends Error {
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+        this.status =`${statusCode}`.startsWith("4") ? "fail" : "error";
+        this.isOperational = true; //all of the error get prop set to true so we can tell the errors apart from programming errors.
+        Error.captureStackTrace(this,this.constructor);
+    }
+}
+
+module.exports = AppError;
