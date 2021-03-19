85e7493f974b2fa6a151582165d842929a0cfbc3
Linus Johansson <linus.react.projects@gmail.com>
2021-03-19 10:43:46 +0100

    added password management

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
+
   const newUser = await User.create(req.body);
 
   res.status(201).json({
--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,48 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
-    name:{
-        type: 'String',
-        required: [true, "A user must have a name"],
+  name: {
+    type: "String",
+    required: [true, "A user must have a name"],
+  },
+  email: {
+    type: "String",
+    required: [true, "A user must have an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  photo: String,
+  password: {
+    type: "String",
+    required: [true, "please enter a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: "String",
+    required: [true, "please confirm your password"],
+    validate: {
+      // This only works on SAVE!
+      validator: function (el) {
+        return el === this.password; // pass123 === pass123
+      },
+      message: "Passwords do not match",
     },
-    email:{
-        type: 'String',
-        required: [true, "A user must have an email"],
-        unique: true,
-        lowercase:true,
-        validate:[validator.isEmail, "Please enter a valid email"]
-    },
-    photo:String,
-    password:{
-        type: 'String',
-        required: [true, "please enter a password"],
-        minlength:8,
-    },
-    passwordConfirm:{
-        type: 'String',
-        required: [true, "please confirm your password"],
-        validate:{
-            // This only works on SAVE!
-            validator:function(el){
-                return el === this.password; // pass123 === pass123
-            }
-        }
-    }
-})
+  },
+});
 
-const User = mongoose.model('User',userSchema)
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was modified
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // delete passwordconfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
-
