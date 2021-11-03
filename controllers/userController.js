93ff7814b8be95c700f798640cf0b17eb0c6d1cb
Linus Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2021-11-03 20:43:51 +0100

    added functionality updating the current user:data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,50 @@
 const User = require("../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+/* Helper function
+ * Loops through all the fields in obj,
+ * each field is checked if it is
+ * one of the allowed fields, if true a new field is created
+ * inside the new object (newObj) with the same field name */
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+// update the currently authenticated user.
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1. create an error if user POSTS password data.
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400
+      )
+    );
+  }
+  //2. filtered out unwanted field names that are not allowed to be updated.
+  const filterdBody = filterObj(req.body, "name", "email");
+
+  //3. Update user document.
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterdBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 // GET all users
-exports.getAllUsers = catchAsync(async (req, res,next) => {
+exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   //SEND RESPONSE
   res.status(200).json({
