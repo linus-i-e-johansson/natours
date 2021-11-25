518fad0259042f0051fcf62c33cd70bd3748149c
Linus Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2021-11-25 12:57:44 +0100

    added rate limiting for better security against DOS and brute force attacks

--- app.js
+++ app.js
@@ -3,17 +3,24 @@
 //
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController")
+const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
-//1) MIDDLEWARES
+//1)GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+/*this will limit the amount of request from the same IP to 100 reqs in 1H */
+const limiter = rateLimit({
+  max: 100,
+  windowMS: 60 * 60 * 1000,
+  message: "To manny request from this IP, please please wait for one hour.",
+});
+app.use("/api", limiter);
 app.use(express.json()); // middleware, needed so that the data from the body is added to the request-obj
 app.use(express.static(`${__dirname}/public`));
 
@@ -27,13 +34,13 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter); // mounting the router
 app.use("/api/v1/users", userRouter); // mounting the router
 
-app.all("*",(req, res, next) => {
-/*
+app.all("*", (req, res, next) => {
+  /*
   const err = new Error(`Cant find ${req.originalUrl} on this server!`);
   err.status = "fail";
   err.statusCode = 404;
   next(err);*/
-  next(new AppError(`Cant find ${req.originalUrl} on this server!`,404));
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
 });
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 module.exports = app;
