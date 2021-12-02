207bbf950a4b07d77bd05c70bcef1d1435de65d3
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2021-12-02 16:43:15 +0100

    check access to the git repository

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,25 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async options =>{
-//1. create transporter.
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+    // 1. create a transporter
+        const transporter =nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth:{
+                user:process.env.EMAIL_USERNAME,
+                password:process.env.USER_PASSWORD
+            }
+        })
+    // 2. define email options
+        const emailOptions = {
+            from:"Linus Johansson <linus.i.e.johansson@gmail.com>",
+            to:options.email,
+            subject:options.subject,
+            text:options.message,
+            //html:
         }
-    });
-//2. Define email options.
-    const emailOptions = {
-        from:"Linus Johansson <linus.i.e.johansson@gmail.com>",
-        to:options.email,
-        subject:options.subject,
-        text: options.message,
-    }
-//3. Actually send email with nodemailer.
-    await transport.sendMail(emailOptions);
-}
+    // 3. send email with nodemailer.
+    await transporter.sendEmail(emailOptions)
+};
 
 module.exports = sendEmail;
