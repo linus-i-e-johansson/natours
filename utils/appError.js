f23fc9a7560df165fbf91b0c2951127b5522f21b
linus-i-e-johansson <67144018+linus-i-e-johansson@users.noreply.github.com>
2022-11-13 00:47:59 +0100

    added refresh

--- utils/appError.js
+++ utils/appError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
         super(message);
         this.statusCode = statusCode;
         this.status =`${statusCode}`.startsWith("4") ? "fail" : "error";
-        this.isOperational = true; //all of the error get prop set to true so we can tell the errors apart from programming errors.
+        this.isOperational = true; //all the error get prop set to true, so we can tell the errors apart from programming errors.
         Error.captureStackTrace(this,this.constructor);
     }
 }
